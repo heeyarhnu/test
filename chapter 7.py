17675759fdae822d4a38d207798c0c087c21ff19
Heeyarhnu <heeyarhnu@gmail.com>
2018-02-03 01:13:41 -0800

    I made a chanhge

--- chapter 7.py
+++ chapter 7.py
@@ -1,8 +1,5 @@
-amount=int(input("enter the amount: "))
-if (amount <= 1000):
-    discount=(amount * 0.05)
-    print ("discount",discount)
-else:
-    discount=(amount*0.10)
-    print ("discount",discount)
-print ("Net payable:",amount - discount)
+age=int(input("enter your age: "))
+if (age>=24):
+    print ("You are an adult")
+elif (age>=18):
+    print ("You are a teenagerhfevfrv")
